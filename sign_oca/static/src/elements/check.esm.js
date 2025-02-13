abf01ddbe4b59a5635a44c087e54b500c9d9f6cc
kobros-tech <alkobroslymohamed@gmail.com>
2025-02-13 05:25:20 +0300

    [FIX] sign_oca: fix sign button interactions

--- sign_oca/static/src/elements/check.esm.js
+++ sign_oca/static/src/elements/check.esm.js
@@ -56,8 +56,8 @@ const checkSignOca = {
         });
         return input;
     },
-    check: function () {
-        return true;
+    check: function (item) {
+        return Boolean(item.value);
     },
 };
 registry.category("sign_oca").add("check", checkSignOca);
