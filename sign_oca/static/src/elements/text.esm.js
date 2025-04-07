f759acdbf9ca0af0dc7c17382bfff9ff4d273d49
kobros-tech <alkobroslymohamed@gmail.com>
2025-04-07 09:59:44 +0300

    [FIX] sign_oca: handle js logic

--- sign_oca/static/src/elements/text.esm.js
+++ sign_oca/static/src/elements/text.esm.js
@@ -50,9 +50,12 @@ const textSignOca = {
                 .sort((a, b) => a.tabindex - b.tabindex);
             if (next_items.length > 0) {
                 ev.currentTarget.blur();
-                parent.items[next_items[0].id].dispatchEvent(
-                    new Event("focus_signature")
-                );
+                const nextItem = next_items[0];
+                if (nextItem && parent.items && parent.items[nextItem.id]) {
+                    parent.items[nextItem.id].dispatchEvent(
+                        new Event("focus_signature")
+                    );
+                }
             }
         });
         return input;
